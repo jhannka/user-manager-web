c88fdd3a6ca46ebc39f4f16bfecf3a3b2a630b31
jhannka <jjhann.meza@gmail.com>
2024-02-24 12:53:42 -0500

    feat: added configuration required

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  url: 'http://localhost:8000/',
+  url_api: 'http://localhost:8000/',
+  client_id: '3',
+  client_secret: 'RgrgEPZjiI9fvZIvmezZikqmRL7invIpgVhe4W8P',
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+  production: false,
+  url: 'https://api.webappdev.soft-g.net/',
+  url_api: 'https://api.webappdev.soft-g.net/api/v1/',
+  url_api_two: 'https://api.webappdev.soft-g.net/api/v2/',
+  grant_type: 'password',
+  client_id: '3',
+  client_secret: 'RgrgEPZjiI9fvZIvmezZikqmRL7invIpgVhe4W8P',
+  scope: '*',
+  maps_api_key: 'AIzaSyBl1cWSargaCazfW3Z_scTDVmBfNNZNISM'
+};
