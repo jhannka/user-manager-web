499af6adca8cad2d7879219d9bb5472cf3b79a95
jhannka <jjhann.meza@gmail.com>
2024-03-01 09:24:10 -0500

    feat: added loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {Subject, takeUntil} from "rxjs";
+import {LoadingState} from "./redux/loading/loading.state";
+import {Store} from "@ngxs/store";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  private destroy: Subject<boolean> = new Subject<boolean>();
+
   title = 'user-manager-web';
+  loading = false;
+
+  constructor(
+    private store: Store
+  ) {
+    this.store.select(LoadingState.getStatus)
+      .pipe(takeUntil(this.destroy))
+      .subscribe((res) => {
+        this.loading = res;
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next(true);
+    this.destroy.unsubscribe();
+  }
 }
